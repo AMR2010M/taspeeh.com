c19a5b8e4dcd95c29dc3d198eae1b57d7bfbcd88
amr512010m <138729544+amr512010m@users.noreply.github.com>
2024-08-25 01:16:02 +0300

    Add files via upload

--- tas.js
+++ tas.js
@@ -3,9 +3,9 @@ var y=document.getElementById('sbtn');
 var r=document.getElementById('rbtn');
 	var d=document.getElementById('d');
 var ul=document.getElementById('ul');
-var t=document.getElementById('t');
-var c=document.getElementById('c');
-var s=document.getElementById('s');
+
+
+
 var alert3=document.getElementById('alert3');
 var a=['violit','blue','red','yellow','brown','orange',]
 var yes=document.getElementById('yes');
@@ -22,26 +22,13 @@ s.style.backgroundColor=a[l]
 i=0;
 var z;
 z=i;
+var tar=0*1;
 
-t.onclick=function(){var e=prompt('enter your target');
-		f=1;			 
-	ne=e*1+f-1;				 
-}
+d.onclick=function(){var tar=prompt('pls,enter target'); num8=tar*1; }
 
 
-ul.style.display='none';
-y.style.height='100px';y.style.width='100px';y.style.borderRadius='100px';y.style.border='none'
-	onkeypress=function(){
-
-		i=i+1;
-	x.innerText=i;
-		if(i>999){x.style.fontSize='30px'}
-y.style.height='100px';y.style.width='100px';y.style.borderRadius='100px';y.style.border='2px solid white'
-	setTimeout(function(){	y.style.border='none';},150);
-y.style.height='100px';y.style.width='100px';y.style.borderRadius='100px';y.style.border='2px solid white'
-	if(i-1==ne-1){window.alert('you achived your target!')}
+y.style.height='100px';y.style.width='100px';y.style.borderRadius='100px';y.style.border='none';
 	
-	}
 y.onclick=function(){
 
 		i=i+1;
@@ -50,7 +37,7 @@ y.onclick=function(){
 y.style.height='100px';y.style.width='100px';y.style.borderRadius='100px';y.style.border='2px solid white'
 	setTimeout(function(){	y.style.border='none';},150);
 y.style.height='100px';y.style.width='100px';y.style.borderRadius='100px';y.style.border='2px solid white'
-	if(i-1==ne-1){window.alert('you achived your target!')}
+	if(i==num8){window.alert('you achived your target!')}
 	
 	}
 var alertt=document.getElementById('alertt');
@@ -66,15 +53,8 @@ alertt.innerText=5
 yes.onclick=function(){ i=0; alert3.style.display='none';x.innerText=i;alertt.innerText=5}
 	no.onclick=function(){alert3.style.display='none';alertt.innerText=5}
 us='t';
-d.onclick=function(){
-	
-	if(us=='t'){ul.style.display='block'; us='f'}
-	else{ul.style.display='none'; us='t'}
-}
-c.onclick=function(){l=l+1;s.style.backgroundColor=a[l];c.innerText=a[l];if(l==a.length-1){l=l-5} }
+
+
 var coco;
 coco=true;
-dd.onclick=function(){
-if(coco==true){main.style.backgroundColor='black'; dd.innerText='lightmode'; coco=false;}
-else{main.style.backgroundColor='white'; dd.innerText='darkmode'; coco=true;}}
 
